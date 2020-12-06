8d69bc68d21301789bb0f08142533f634aa54fa6
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-12-06 18:44:28 +0100

    update release.sh

--- release.sh
+++ release.sh
@@ -20,23 +20,22 @@
 echo "===================================="
 read -p "Enter Release: " releaseVersion
 read -p "Enter Next Release (SNAPSHOT): " nextVersion
-read -p "Enter Github Username: " username
-read -s -p "Enter Github Password: " password
+read -s -p "Enter Github Token: " token
 
 # Update to release
 mvn versions:set -DnewVersion="$releaseVersion"
 
 git add .
 git commit -m "Release $releaseVersion"
-git push "https://$username:$password@github.com/project-openubl/xbuilder.git"
+git push "https://$token@github.com/project-openubl/xbuilder.git"
 
 # Create tag and push
 git tag "$releaseVersion"
-git push "https://$username:$password@github.com/project-openubl/xbuilder.git" --tags
+git push "https://$token@github.com/project-openubl/xbuilder.git" --tags
 
 # Create next snapshot
 mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
 git commit -m "Prepare next release $nextVersion-SNAPSHOT"
-git push "https://$username:$password@github.com/project-openubl/xbuilder.git"
+git push "https://$token@github.com/project-openubl/xbuilder.git"
