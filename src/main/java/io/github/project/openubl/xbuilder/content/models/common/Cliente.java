bc00a2ae8a9ea3897cd9bba5db334f4c10857a8e
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-04-04 19:05:27 +0200

    Add docs - cliente (#65)
    * Add docs - cliente
    
    * format

--- src/main/java/io/github/project/openubl/xbuilder/content/models/common/Cliente.java
+++ src/main/java/io/github/project/openubl/xbuilder/content/models/common/Cliente.java
@@ -16,17 +16,47 @@
  */
 package io.github.project.openubl.xbuilder.content.models.common;
 
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog6;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Cliente de la operación.
+ *
+ * @author <a href="mailto:carlosthe19916@gmail.com">Carlos Feria</a>
+ */
 @Data
 @Builder
 public class Cliente {
 
+    /**
+     * Tipo de documento de identidad del cliente.
+     * <p>
+     * Catalogo 06.
+     * <p>
+     * Valores válidos: {@link Catalog6}
+     */
     private String tipoDocumentoIdentidad;
+
+    /**
+     * Número de documento de identidad del cliente.
+     */
     private String numeroDocumentoIdentidad;
+
+    /**
+     * Nombre del cliente. Si el cliente es personal natural entonces
+     * es el nombre y apellidos de la persona; si el cliente es una persona jurídica
+     * entonces es la razón social de la empresa.
+     */
     private String nombre;
 
+    /**
+     * Dirección del cliente
+     */
     private Direccion direccion;
+
+    /**
+     * Datos de contacto del cliente
+     */
     private Contacto contacto;
 }
