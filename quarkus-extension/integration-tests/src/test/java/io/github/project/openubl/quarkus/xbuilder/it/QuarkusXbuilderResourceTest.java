6c6ee38e6c4bfab49b68f8450b167c62bb33a8a8
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2022-12-22 07:43:18 +0100

    Test deserialization

--- quarkus-extension/integration-tests/src/test/java/io/github/project/openubl/quarkus/xbuilder/it/QuarkusXbuilderResourceTest.java
+++ quarkus-extension/integration-tests/src/test/java/io/github/project/openubl/quarkus/xbuilder/it/QuarkusXbuilderResourceTest.java
@@ -16,25 +16,74 @@
  */
 package io.github.project.openubl.quarkus.xbuilder.it;
 
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog1;
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog19;
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog1_Invoice;
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog22;
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog23;
+import io.github.project.openubl.xbuilder.content.catalogs.Catalog6;
+import io.github.project.openubl.xbuilder.content.models.common.Cliente;
+import io.github.project.openubl.xbuilder.content.models.common.Proveedor;
+import io.github.project.openubl.xbuilder.content.models.standard.general.CreditNote;
+import io.github.project.openubl.xbuilder.content.models.standard.general.DebitNote;
+import io.github.project.openubl.xbuilder.content.models.standard.general.DocumentoVentaDetalle;
+import io.github.project.openubl.xbuilder.content.models.standard.general.Invoice;
+import io.github.project.openubl.xbuilder.content.models.sunat.baja.VoidedDocuments;
+import io.github.project.openubl.xbuilder.content.models.sunat.baja.VoidedDocumentsItem;
+import io.github.project.openubl.xbuilder.content.models.sunat.percepcionretencion.PercepcionRetencionOperacion;
+import io.github.project.openubl.xbuilder.content.models.sunat.percepcionretencion.Perception;
+import io.github.project.openubl.xbuilder.content.models.sunat.percepcionretencion.Retention;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.Comprobante;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.ComprobanteAfectado;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.ComprobanteImpuestos;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.ComprobanteValorVenta;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.SummaryDocuments;
+import io.github.project.openubl.xbuilder.content.models.sunat.resumen.SummaryDocumentsItem;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
-
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testInvoice() {
+        Invoice invoice = Invoice.builder()
+                .serie("F001")
+                .numero(1)
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .cliente(Cliente.builder()
+                        .nombre("Carlos Feria")
+                        .numeroDocumentoIdentidad("12121212121")
+                        .tipoDocumentoIdentidad(Catalog6.RUC.toString())
+                        .build()
+                )
+                .detalle(DocumentoVentaDetalle.builder()
+                        .descripcion("Item1")
+                        .cantidad(new BigDecimal("10"))
+                        .precio(new BigDecimal("100"))
+                        .build()
+                )
+                .build();
+
         given()
                 .when()
-                .get("/quarkus-xbuilder/invoice")
+                .contentType(ContentType.JSON)
+                .body(invoice)
+                .post("/quarkus-xbuilder/invoice")
                 .then()
                 .statusCode(200)
-                .body(
-                        is(
+                .body(is(
                                 "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
                                         "<Invoice xmlns=\"urn:oasis:names:specification:ubl:schema:xsd:Invoice-2\"\n" +
                                         "         xmlns:cac=\"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2\"\n" +
@@ -164,13 +213,38 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testCreditNote() {
+        CreditNote creditNote = CreditNote.builder()
+                .serie("FC01")
+                .numero(1)
+                .comprobanteAfectadoSerieNumero("F001-1")
+                .sustentoDescripcion("mi sustento")
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .cliente(Cliente.builder()
+                        .nombre("Carlos Feria")
+                        .numeroDocumentoIdentidad("12121212121")
+                        .tipoDocumentoIdentidad(Catalog6.RUC.toString())
+                        .build()
+                )
+                .detalle(DocumentoVentaDetalle.builder()
+                        .descripcion("Item1")
+                        .cantidad(new BigDecimal("10"))
+                        .precio(new BigDecimal("100"))
+                        .build()
+                )
+                .build();
+
         given()
                 .when()
-                .get("/quarkus-xbuilder/credit-note")
+                .contentType(ContentType.JSON)
+                .body(creditNote)
+                .post("/quarkus-xbuilder/credit-note")
                 .then()
                 .statusCode(200)
-                .body(
-                        is(
+                .body(is(
                                 "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
                                         "<CreditNote xmlns=\"urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2\"\n" +
                                         "         xmlns:cac=\"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2\"\n" +
@@ -305,13 +379,38 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testDebitNote() {
+        DebitNote debitNote = DebitNote.builder()
+                .serie("FD01")
+                .numero(1)
+                .comprobanteAfectadoSerieNumero("F001-1")
+                .sustentoDescripcion("mi sustento")
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .cliente(Cliente.builder()
+                        .nombre("Carlos Feria")
+                        .numeroDocumentoIdentidad("12121212121")
+                        .tipoDocumentoIdentidad(Catalog6.RUC.toString())
+                        .build()
+                )
+                .detalle(DocumentoVentaDetalle.builder()
+                        .descripcion("Item1")
+                        .cantidad(new BigDecimal("10"))
+                        .precio(new BigDecimal("100"))
+                        .build()
+                )
+                .build();
+
         given()
                 .when()
-                .get("/quarkus-xbuilder/debit-note")
+                .contentType(ContentType.JSON)
+                .body(debitNote)
+                .post("/quarkus-xbuilder/debit-note")
                 .then()
                 .statusCode(200)
-                .body(
-                        is(
+                .body(is(
                                 "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
                                         "<DebitNote xmlns=\"urn:oasis:names:specification:ubl:schema:xsd:DebitNote-2\"\n" +
                                         "         xmlns:cac=\"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2\"\n" +
@@ -446,8 +545,36 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testVoidedDocuments() {
+        VoidedDocuments voidedDocuments = VoidedDocuments.builder()
+                .numero(1)
+                .fechaEmision(LocalDate.of(2022, 01, 31))
+                .fechaEmisionComprobantes(LocalDate.of(2022, 01, 29))
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .comprobante(VoidedDocumentsItem.builder()
+                        .serie("F001")
+                        .numero(1)
+                        .tipoComprobante(Catalog1_Invoice.FACTURA.getCode())
+                        .descripcionSustento("Mi sustento1")
+                        .build()
+                )
+                .comprobante(VoidedDocumentsItem.builder()
+                        .serie("F001")
+                        .numero(2)
+                        .tipoComprobante(Catalog1_Invoice.FACTURA.getCode())
+                        .descripcionSustento("Mi sustento2")
+                        .build()
+                )
+                .build();
+
         given()
-                .when().get("/quarkus-xbuilder/voided-documents")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(voidedDocuments)
+                .post("/quarkus-xbuilder/voided-documents")
                 .then()
                 .statusCode(200)
                 .body(is("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
@@ -512,8 +639,76 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testSummaryDocuments() {
+        SummaryDocuments summaryDocuments = SummaryDocuments.builder()
+                .numero(1)
+                .fechaEmision(LocalDate.of(2022, 01, 31))
+                .fechaEmisionComprobantes(LocalDate.of(2022, 01, 29))
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .comprobante(SummaryDocumentsItem.builder()
+                        .tipoOperacion(Catalog19.ADICIONAR.toString())
+                        .comprobante(Comprobante.builder()
+                                .tipoComprobante(Catalog1_Invoice.BOLETA.getCode())//
+                                .serieNumero("B001-1")
+                                .cliente(Cliente.builder()
+                                        .nombre("Carlos Feria")
+                                        .numeroDocumentoIdentidad("12345678")
+                                        .tipoDocumentoIdentidad(Catalog6.DNI.getCode())
+                                        .build()
+                                )
+                                .impuestos(ComprobanteImpuestos.builder()
+                                        .igv(new BigDecimal("18"))
+                                        .icb(new BigDecimal(2))
+                                        .build()
+                                )
+                                .valorVenta(ComprobanteValorVenta.builder()
+                                        .importeTotal(new BigDecimal("120"))
+                                        .gravado(new BigDecimal("120"))
+                                        .build()
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .comprobante(SummaryDocumentsItem.builder()
+                        .tipoOperacion(Catalog19.ADICIONAR.toString())
+                        .comprobante(Comprobante.builder()
+                                .tipoComprobante(Catalog1.NOTA_CREDITO.getCode())
+                                .serieNumero("BC02-2")
+                                .comprobanteAfectado(ComprobanteAfectado.builder()
+                                        .serieNumero("B002-2")
+                                        .tipoComprobante(Catalog1.BOLETA.getCode()) //
+                                        .build()
+                                )
+                                .cliente(Cliente.builder()
+                                        .nombre("Carlos Feria")
+                                        .numeroDocumentoIdentidad("12345678")
+                                        .tipoDocumentoIdentidad(Catalog6.DNI.getCode())//
+                                        .build()
+                                )
+                                .impuestos(ComprobanteImpuestos.builder()
+                                        .igv(new BigDecimal("18"))
+                                        .build()
+                                )
+                                .valorVenta(ComprobanteValorVenta.builder()
+                                        .importeTotal(new BigDecimal("118"))
+                                        .gravado(new BigDecimal("118"))
+                                        .build()
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build();
+
         given()
-                .when().get("/quarkus-xbuilder/summary-documents")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(summaryDocuments)
+                .post("/quarkus-xbuilder/summary-documents")
                 .then()
                 .statusCode(200)
                 .body(is("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
@@ -646,8 +841,46 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testPerception() {
+        Perception perception = Perception.builder()
+                .serie("P001")
+                .numero(1)
+                .fechaEmision(LocalDate.of(2022, 01, 31))
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .cliente(Cliente.builder()
+                        .nombre("Carlos Feria")
+                        .numeroDocumentoIdentidad("12121212121")
+                        .tipoDocumentoIdentidad(Catalog6.RUC.getCode())
+                        .build()
+                )
+                .importeTotalPercibido(new BigDecimal("10"))
+                .importeTotalCobrado(new BigDecimal("210"))
+                .tipoRegimen(Catalog22.VENTA_INTERNA.getCode())
+                .tipoRegimenPorcentaje(Catalog22.VENTA_INTERNA.getPercent()) //
+                .operacion(PercepcionRetencionOperacion.builder()
+                        .numeroOperacion(1)
+                        .fechaOperacion(LocalDate.of(2022, 01, 31))
+                        .importeOperacion(new BigDecimal("100"))
+                        .comprobante(io.github.project.openubl.xbuilder.content.models.sunat.percepcionretencion.ComprobanteAfectado.builder()
+                                .tipoComprobante(Catalog1.FACTURA.getCode())
+                                .serieNumero("F001-1")
+                                .fechaEmision(LocalDate.of(2022, 01, 31))
+                                .importeTotal(new BigDecimal("200"))
+                                .moneda("PEN")
+                                .build()
+                        )
+                        .build()
+                )
+                .build();
+
         given()
-                .when().get("/quarkus-xbuilder/perception")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(perception)
+                .post("/quarkus-xbuilder/perception")
                 .then()
                 .statusCode(200)
                 .body(is("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
@@ -726,8 +959,46 @@ public class QuarkusXbuilderResourceTest {
 
     @Test
     public void testRetention() {
+        Retention retention = Retention.builder()
+                .serie("R001")
+                .numero(1)
+                .fechaEmision(LocalDate.of(2022, 01, 31))
+                .proveedor(Proveedor.builder()
+                        .ruc("12345678912")
+                        .razonSocial("Softgreen S.A.C.")
+                        .build()
+                )
+                .cliente(Cliente.builder()
+                        .nombre("Carlos Feria")
+                        .numeroDocumentoIdentidad("12121212121")
+                        .tipoDocumentoIdentidad(Catalog6.RUC.getCode())
+                        .build()
+                )
+                .importeTotalRetenido(new BigDecimal("10"))
+                .importeTotalPagado(new BigDecimal("200"))
+                .tipoRegimen(Catalog23.TASA_TRES.getCode())
+                .tipoRegimenPorcentaje(Catalog23.TASA_TRES.getPercent()) //
+                .operacion(PercepcionRetencionOperacion.builder()
+                        .numeroOperacion(1)
+                        .fechaOperacion(LocalDate.of(2022, 01, 31))
+                        .importeOperacion(new BigDecimal("100"))
+                        .comprobante(io.github.project.openubl.xbuilder.content.models.sunat.percepcionretencion.ComprobanteAfectado.builder()
+                                .tipoComprobante(Catalog1.FACTURA.getCode())
+                                .serieNumero("F001-1")
+                                .fechaEmision(LocalDate.of(2022, 01, 31))
+                                .importeTotal(new BigDecimal("210"))
+                                .moneda("PEN")
+                                .build()
+                        )
+                        .build()
+                )
+                .build();
+
         given()
-                .when().get("/quarkus-xbuilder/retention")
+                .when()
+                .contentType(ContentType.JSON)
+                .body(retention)
+                .post("/quarkus-xbuilder/retention")
                 .then()
                 .statusCode(200)
                 .body(is("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
