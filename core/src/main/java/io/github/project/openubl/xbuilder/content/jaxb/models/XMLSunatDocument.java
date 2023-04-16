39a1f14dcf90c118b041b49e0a95bb433dd76bd8
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2023-04-16 17:40:17 +0200

    chore: Jaxb add xml types (#223)

--- core/src/main/java/io/github/project/openubl/xbuilder/content/jaxb/models/XMLSunatDocument.java
+++ core/src/main/java/io/github/project/openubl/xbuilder/content/jaxb/models/XMLSunatDocument.java
@@ -23,9 +23,11 @@ import lombok.NoArgsConstructor;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.LocalDate;
 
+@XmlType(name = "SunatDocument")
 @XmlAccessorType(XmlAccessType.NONE)
 @Data
 @NoArgsConstructor
