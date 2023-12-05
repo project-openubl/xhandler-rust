a0eabd76a0241b864272fac438f2f650a82d6e8e
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-12-05 22:08:31 +0100

    Format

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,8 +1,7 @@
 pub use crate::catalogs::*;
 pub use crate::enricher::enrich::{Defaults, EnrichTrait};
-pub use crate::models::*;
 pub use crate::models::common::*;
 pub use crate::models::general::*;
 pub use crate::models::invoice::*;
+pub use crate::models::*;
 pub use crate::renderer::render_invoice;
-
