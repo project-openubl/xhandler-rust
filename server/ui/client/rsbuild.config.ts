1fb8e8c948727179d121b9f7698a6d85042cde12
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-03-02 15:35:53 +0100

    remove error with require
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- server/ui/client/rsbuild.config.ts
+++ server/ui/client/rsbuild.config.ts
@@ -5,66 +5,120 @@ import { type RsbuildPlugin, defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { TanStackRouterRspack } from "@tanstack/router-plugin/rspack";
 
-/**
- * Return a base64 encoded JSON string containing the given `env` object.
- */
-export const encodeEnv = (env: object, exclude?: string[]): string => {
-  const filtered = exclude
-    ? Object.fromEntries(
-        Object.entries(env).filter(([key]) => !exclude.includes(key)),
-      )
-    : env;
+import {
+	brandingStrings,
+	encodeEnv,
+	proxyMap,
+	SERVER_ENV_KEYS,
+	OPENUBL_ENV,
+	brandingAssetPath,
+} from "@openubl-ui/common";
 
-  return btoa(JSON.stringify(filtered));
-};
+const brandingPath: string = brandingAssetPath();
+const manifestPath = path.resolve(brandingPath, "manifest.json");
+const faviconPath = path.resolve(brandingPath, "favicon.ico");
 
 export const copyIndex = (): RsbuildPlugin => ({
-  name: "CopyIndex",
-  setup(api) {
-    if (process.env.NODE_ENV === "production") {
-      api.onAfterBuild(() => {
-        const distDir = path.resolve(__dirname, "dist");
-        const src = path.join(distDir, "index.html");
-        const dest = path.join(distDir, "index.html.ejs");
+	name: "CopyIndex",
+	setup(api) {
+		if (process.env.NODE_ENV === "production") {
+			api.onAfterBuild(() => {
+				const distDir = path.resolve(__dirname, "dist");
+				const src = path.join(distDir, "index.html");
+				const dest = path.join(distDir, "index.html.ejs");
 
-        if (fs.existsSync(src)) {
-          fs.renameSync(src, dest);
-        }
-      });
-    }
-  },
+				if (fs.existsSync(src)) {
+					fs.renameSync(src, dest);
+				}
+			});
+		}
+	},
+});
+
+export const ignoreProcessEnv = (): RsbuildPlugin => ({
+	name: "ignore-process-env",
+	setup(api) {
+		if (process.env.NODE_ENV === "development") {
+			api.transform({ test: /\.mjs$/ }, ({ code, resourcePath }) => {
+				let newCode = code;
+				if (
+					code.includes("process.env") &&
+					resourcePath.includes("/common/dist/index.mjs")
+				) {
+					newCode = code.replace(/process\.env/g, "({})");
+				}
+				return newCode;
+			});
+		}
+		if (process.env.NODE_ENV === "production") {
+			api.onAfterBuild(() => {
+				const replaceProcessEnv = (dir: string): void => {
+					const files = fs.readdirSync(dir);
+					for (const file of files) {
+						const filePath = path.join(dir, file);
+						const fileStat = fs.statSync(filePath);
+						if (fileStat?.isDirectory()) {
+							replaceProcessEnv(filePath);
+						} else if (file.endsWith(".js")) {
+							let code = fs.readFileSync(filePath, "utf-8");
+							code = code.replace(/process\.env/g, "({})");
+							fs.writeFileSync(filePath, code);
+						}
+					}
+				};
+
+				const distDir = path.resolve(__dirname, "dist");
+				replaceProcessEnv(distDir);
+			});
+		}
+	},
 });
 
 export default defineConfig({
-  plugins: [pluginReact(), copyIndex()],
-  html: {
-    template: path.join(__dirname, "index.html"),
-    templateParameters: {
-      ...(process.env.NODE_ENV === "development" && {
-        _env: encodeEnv({ version: "99.0.0" }),
-      }),
-    },
-    title: "Openubl",
-    favicon: "./src/assets/favicon.svg",
-  },
-  tools: {
-    rspack(_config, { addRules, appendPlugins }) {
-      addRules([
-        ...(process.env.NODE_ENV === "production"
-          ? [
-              {
-                test: /\.html$/,
-                use: "raw-loader", // Ensures HTML remains unprocessed
-              },
-            ]
-          : []),
-      ]);
-      appendPlugins([
-        TanStackRouterRspack({ target: "react", autoCodeSplitting: true }),
-      ]);
-    },
-  },
-  output: {
-    manifest: true,
-  },
+	plugins: [pluginReact(), copyIndex(), ignoreProcessEnv()],
+	html: {
+		template: path.join(__dirname, "index.html"),
+		templateParameters: {
+			...(process.env.NODE_ENV === "development" && {
+				_env: encodeEnv(OPENUBL_ENV, SERVER_ENV_KEYS),
+				branding: brandingStrings,
+			}),
+		},
+	},
+	tools: {
+		rspack(_config, { addRules, appendPlugins }) {
+			addRules([
+				...(process.env.NODE_ENV === "production"
+					? [
+							{
+								test: /\.html$/,
+								use: "raw-loader", // Ensures HTML remains unprocessed
+							},
+						]
+					: []),
+			]);
+			appendPlugins([
+				TanStackRouterRspack({ target: "react", autoCodeSplitting: true }),
+			]);
+		},
+	},
+	output: {
+		copy: [
+			{
+				from: manifestPath,
+				to: ".",
+			},
+			{
+				from: brandingPath,
+				to: ".",
+			},
+			{
+				from: faviconPath,
+				to: ".",
+			},
+		],
+	},
+	server: {
+		proxy: proxyMap,
+	},
 });
--- server/ui/common/src/branding.ts
+++ server/ui/common/src/branding.ts
@@ -1,33 +1,33 @@
 export interface MastheadBrand {
-  src: string;
-  alt: string;
-  height: string;
+	src: string;
+	alt: string;
+	height: string;
 }
 
 export interface MastheadTitle {
-  text: string;
-  heading?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  size?: "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
+	text: string;
+	heading?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+	size?: "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
 }
 
 export interface BrandingStrings {
-  application: {
-    title: string;
-    name?: string;
-    description?: string;
-  };
+	application: {
+		title: string;
+		name?: string;
+		description?: string;
+	};
 
-  about: {
-    displayName: string;
-    imageSrc?: string;
-    documentationUrl?: string;
-  };
+	about: {
+		displayName: string;
+		imageSrc?: string;
+		documentationUrl?: string;
+	};
 
-  masthead: {
-    leftBrand?: MastheadBrand;
-    leftTitle?: MastheadTitle;
-    rightBrand?: MastheadBrand;
-  };
+	masthead: {
+		leftBrand?: MastheadBrand;
+		leftTitle?: MastheadTitle;
+		rightBrand?: MastheadBrand;
+	};
 }
 
 // Note: Typescript will look at the `paths` definition to resolve this import
@@ -37,12 +37,15 @@ export interface BrandingStrings {
 import * as stringsJson from "@branding/strings.json";
 
 export const brandingStrings =
-  stringsJson.default as unknown as BrandingStrings;
+	stringsJson.default as unknown as BrandingStrings;
 
 /**
  * Return the `node_modules/` resolved path for the branding assets.
  */
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
 export const brandingAssetPath = () =>
-  `${require
-    .resolve("@openubl-ui/common/package.json")
-    .replace(/(.)\/package.json$/, "$1")}/dist/branding`;
+	`${require
+		.resolve("@openubl-ui/common/package.json")
+		.replace(/(.)\/package.json$/, "$1")}/dist/branding`;
